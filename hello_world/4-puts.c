e29ab6b68f5047d9e82d15a6553593538aa6168a
sindi16 <10029@holbertonstudents.com>
2024-10-21 10:57:54 +0000

    compile

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,7 @@
-include <stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-   printf("Programming is like building a multilingual puzzle/n");
-   return (0)
-
+   puts("Programming is like building a multilingual puzzle");
+   return (0);
 }
-
