3bb514655b0874db4a513eb6e2a6cb89fdb6c8cb
sindi16 <10029@holbertonstudents.com>
2024-10-22 21:48:00 +0000

    exercises

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i <= 9; i++);
+    {
+        printf(i + '0'\n); // Convert integer to character
+    }
+
+    return (0);
+}
