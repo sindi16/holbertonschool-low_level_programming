5e8735e7502fea2a2298911e7cfaae8e8ce369c1
sindi16 <10029@holbertonstudents.com>
2024-10-23 11:06:28 +0000

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char = 'a';
+char letter = 'a';
 
 while(letter <= 'z')
 {
@@ -15,7 +15,7 @@ while(letter <= 'z')
     {
         putchar(letter);
     }
-    letter++
+    letter++;
 }
 putchar('\n');
 
--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -13,7 +13,7 @@ int main(void)
     {
         printf(i + '0'\n); // Convert integer to character
     }
-    putchar(''\n);
+    putchar('\n');
 
     return (0);
 }
