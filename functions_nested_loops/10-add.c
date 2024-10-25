0dd3bde1201c388f1e8829e45f55214fe709d9ff
sindi16 <10029@holbertonstudents.com>
2024-10-25 09:32:06 +0000

    sum of 2 int

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - sum of two integers and returns the result
+ * @a : holds value
+ * @b : holds value
+ * Return: (a + b)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
