58c82f7775c829aff98b58c009e4bfc9ca175138
sindi16 <10029@holbertonstudents.com>
2024-10-29 11:54:14 +0000

    prints the numbers, from 0 to 9

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints the numbers.
+*/
+
+void print_most_numbers(void)
+{	
+	int n;
+	
+	for (n = 0; n <= 9; n++)
+	{
+		if(n != 2 && n != 4)
+		{
+			_putchar( '0'+ n);
+		}
+	}
+	_putchar('\n');
+}
