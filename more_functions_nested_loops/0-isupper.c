b3d5a039f59b5e4094fc5420e2605278b69e5d0e
sindi16 <10029@holbertonstudents.com>
2024-10-29 22:32:46 +0000

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,19 @@
-#include "main.h"
-#include <stdio.h>
+#include"main.h"
 
 /**
-* _isupper - function to check ascii
-*@c: input value
-* Return: 0 or 1
-*/
+ * _isupper - check for uppercase letters
+ * @c: -variable int c check character
+ * Return: 0 or 1
+ */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	return (1);
+	if ((c >= 65 && c <= 90))
+	{
+		return (1);
+	}
 	else
+	{
 		return (0);
+	}
+
 }
