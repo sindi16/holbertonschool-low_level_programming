84e9aedc2d244c75c5254e2c1764546f848b4d49
sindi16 <10029@holbertonstudents.com>
2024-10-29 09:41:55 +0000

    jnjk

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+
+
+/**
+* _isupper - function to check ascii
+* @c: input value
+* Return: 0 or 1
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+int _isupper(int c);
