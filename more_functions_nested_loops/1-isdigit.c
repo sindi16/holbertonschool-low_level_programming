e393567db1084e20d04e5646fb68e16972f49395
sindi16 <10029@holbertonstudents.com>
2024-10-29 12:31:15 +0000

    prints the numbers, from 0 to 9

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -7,4 +7,11 @@
 * Return: 0 or 1
 */
 
-int 
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	
+	else
+		return (0);
+}
--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,12 @@
 void print_most_numbers(void)
 {	
 	int n;
-	
+
 	for (n = 0; n <= 9; n++)
 	{
 		if(n != 2 && n != 4)
 		{
-			_putchar( '0'+ n);
+			_putchar('0' + n);
 		}
 	}
 	_putchar('\n');
