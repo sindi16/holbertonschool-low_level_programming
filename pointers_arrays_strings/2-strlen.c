975c7f7a3aa23c7f9418fcf3e2675aa0686d2dc6
sindi16 <10029@holbertonstudents.com>
2024-10-30 11:59:44 +0000

    returns the length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* strlen - returns the length of a string
+* @s - points to the string
+* Return: value
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i ++)
+	{
+
+	}
+	return (i);
+}
