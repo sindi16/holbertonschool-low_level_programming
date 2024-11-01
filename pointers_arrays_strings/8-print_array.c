4784845f96d8990d0b126ebeec442c75ea032f1e
sindi16 <10029@holbertonstudents.com>
2024-11-01 06:01:13 +0000

    prints n elements of an array of integers

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,24 +1,23 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * print_square - function print line
- * @size: - parameter from input until when print \
- * main.h header file
- * Return: Void
- */
-
-void print_square(int size)
+* print_array - Prints n elements of an array of integers,
+* followed by a new line.
+* @a: The array of integers.
+* @n: The number of elements to print.
+*/
+void print_array(int *a, int n)
 {
-	int i, j;
+int i;
 
-	if (size > 0)
+for (i = 0; i < n; i++)
+{
+	if (i != 0)
 	{
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-				_putchar('#');
-			_putchar('\n');
-		}
+		printf(", ");
 	}
-	else
-		_putchar('\n');
+	printf("%d", a[i]);
+}
+printf("\n");
 }
