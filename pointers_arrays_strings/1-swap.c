1f4e9dcb02808b615f9fbb7da6c3f97cc4043649
sindi16 <10029@holbertonstudents.com>
2024-11-01 05:47:45 +0000

    swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
-* swap_int - swaps to value
-* @a - points to value
-* @b - points to value
+* swap_int - swaps two values
+*@a: points to value
+*@b: points to value
 */
-
 
 void swap_int(int *a, int *b)
 {
 	int c;
 
-	c = *a;
-	*a = *b;
-	*b = c;
+	c = *b;
+	*b = *a;
+	*a = c;
 }
