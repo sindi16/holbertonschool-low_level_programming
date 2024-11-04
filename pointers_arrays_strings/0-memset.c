685875703e51da584ac54690020bf23b42796af8
sindi16 <10029@holbertonstudents.com>
2024-11-04 18:45:54 +0000

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,14 @@
-#include "main.h"
+#include"main.h"
 
 /**
-* _memset -fills memory 
-* @s: points to the memory
-* @b: char to put
-* @n: number of times to insert the char
-*/
+ * _memset -entry point,fills memory
+ * @s: -points to specific array
+ * @b: -holds value for output
+ * @n: -holds value for output
+ * Return: (s)
+ */
+
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
@@ -13,6 +16,8 @@ char *_memset(char *s, char b, unsigned int n)
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
+
 	}
 	return (s);
+
 }
