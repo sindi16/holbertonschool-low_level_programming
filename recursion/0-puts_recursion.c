17270649ad618dadff401a6661a15bf1fc256be3
sindi16 <10029@holbertonstudents.com>
2024-11-06 13:06:52 +0000

    adding

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,15 +5,16 @@
  * @s: a string
  * Return: void
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
-	else
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
