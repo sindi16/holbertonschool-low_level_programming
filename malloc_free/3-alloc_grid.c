ebc7a7425d221775eb70467c8ac2559eb7825c79
sindi16 <10029@holbertonstudents.com>
2024-11-11 23:55:47 +0000

    push

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+* *str_concat - a function that concatenates two strings
+*
+*
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+
+}
