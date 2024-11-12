4eabb3e61b734fa50154d2f87509527fef98adf4
sindi16 <10029@holbertonstudents.com>
2024-11-12 00:15:14 +0000

    push

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,9 +2,13 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+* *str_concat - a function that concatenates two strings
+*
+*
+*/
+
 char *str_concat(char *s1, char *s2)
 {
 
-	
 }
-
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
