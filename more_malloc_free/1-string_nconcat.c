28fb032ec5875ede012f96610c431b677f50c6fd
sindi16 <10029@holbertonstudents.com>
2024-11-12 13:07:37 +0000

    added file

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -21,8 +21,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
+	{
+
+	}
 
 	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{
+
+	}
 
 	if (n < len2)
 		len2 = n;
