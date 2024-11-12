b0f5c9e10d8fa0be5ecc43447ce7bfe9a2b663d3
sindi16 <10029@holbertonstudents.com>
2024-11-12 11:09:36 +0000

    i

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* *malloc_checked -  allocates memory using
+* @b: size in byte
+* Return: a pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
