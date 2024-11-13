2f8cab69be2da87db6bd2d2c6f56c5fe008af6a8
sindi16 <10029@holbertonstudents.com>
2024-11-13 12:39:38 +0000

    structer for a dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char * owner;
+};
+
+#endif
