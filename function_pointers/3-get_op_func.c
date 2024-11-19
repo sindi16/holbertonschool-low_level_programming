4f7a3f346ae22d6bb892428329617361e357b5d8
sindi16 <10029@holbertonstudents.com>
2024-11-19 10:42:24 +0000

    hh

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,4 +1,4 @@
-#include ""3-calc.h"
+#include "3-calc.h"
 #include <stdlib.h>
 
 int (*get_op_func(char *s))(int, int);
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,14 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, int argv)
+int main(int argc, char* argv)
 {
 	int num1, num2;
 	char* op;
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+	}
 	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit (99);
+	}
+
+	if ((*op == '/' || *op == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
 	printf("%d\n", get_op_func(op)(num1, num2));
 
