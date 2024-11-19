db2f09c6013a2e749251ae514c35526b80d4e765
sindi16 <10029@holbertonstudents.com>
2024-11-19 12:38:50 +0000

    hh

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,22 +1,32 @@
-#include "3-calc.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "3-calc.h"
 
-int (*get_op_func(char *s))(int, int);
+/**
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the user
+ * @s: operator passed as argument to the program
+ * Return: a pointer to the function that corresponds to
+ * the operator given as a parameter
+ */
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-	i = 0;
+	while (ops[i].op != NULL)
 	{
-	while (ops[i].op == *s)
-		return (*ops[i].f);
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
 		i++;
 	}
 	return (NULL);
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,33 +1,35 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char* argv)
+/**
+ * main - Entry point. Performs basic arithmetic operations on two numbers.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0 (Success).
+ */
+
+int main(int argc, char *argv[])
 {
 	int num1, num2;
-	char* op;
+	int (*op)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
+		exit(98);
 	}
 	num1 = atoi(argv[1]);
-	op = argv[2];
 	num2 = atoi(argv[3]);
-
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	op = get_op_func(argv[2]);
+	if (op == NULL)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
-
-	if ((*op == '/' || *op == '%') && num2 == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	printf("%d\n", get_op_func(op)(num1, num2));
-
+	printf("%d\n", op(num1, num2));
 	return (0);
 }
