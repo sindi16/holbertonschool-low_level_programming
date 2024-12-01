ef1b9ab035b12bd6e77b6074b9964d63747b903b
sindi16 <10029@holbertonstudents.com>
2024-12-01 09:57:06 +0000

    starting

---
+++ BankAccount/bank.c
@@ -0,0 +1,16 @@
+#include "bank.h"
+#include <stdio.h>
+
+//function to see the balance of a client
+
+void viewBalance(BankClient clients[], int size, int accountNumber)
+{
+	int i;
+	for(int = 0; i < size; i++)
+	{
+		if (clients[i].accountNumber == accountNumber)
+		{
+			printf("Account bala")
+		}
+	}
+}
---
+++ BankAccount/bank.h
@@ -0,0 +1,17 @@
+#ifndef BANK_H
+#define BANK_H
+
+typedef BankClient
+{
+    char name[100];
+    int accountNumber;
+    float balance;
+} BankClient;
+
+void viewBalance(BankClient clients[], int size, int accountNumber);
+void deposit(BankClient clients[], int size, int accountNumber, float amount);
+void withdraw(BankClient clients[], int size, int accountNumber, float amount);
+
+
+
+#endif BANK_H 
